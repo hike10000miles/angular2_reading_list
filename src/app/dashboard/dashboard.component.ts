1621189171ada678fe686ae182e82001fb55822e
Chen Shen <chenshen4009@gmail.com>
2017-05-23 23:41:09 -0400

    change dashboard

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -2,6 +2,7 @@ import { BookService } from './../book.service';
 import { BookListService } from './../bookList.service';
 import { BookList } from './../IBookList';
 import { Component, OnInit } from '@angular/core';
+import { Book } from './../IBook';
 
 @Component({
   selector: 'app-dashboard',
@@ -10,14 +11,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit{
     lists: BookList[] = new Array<BookList>();
+    mostPopularBooks: Book[] = new Array<Book>();
 
     constructor(
         private bookListService: BookListService,
         private bookService: BookService
     ) { }
 
+    addToMostPopularBooks(books: Book[]): void {
+        let me = this;
+        books = books.slice(0, 4);
+        books.forEach(function(book) {
+            me.mostPopularBooks.push(book);
+        });
+    }
+
+    getMostPopularBooks(list: BookList[]): void {
+        let bookList = list.slice(0, 2);
+        let me = this;
+        bookList.forEach(function(list) {
+            me.bookService.getBooks(list.list_name)
+              .then(books => me.addToMostPopularBooks(books));
+        });
+    }
+
     ngOnInit(): void {
         this.bookListService.getBooks()
-            .then(bookLists => this.lists = bookLists.slice(0, 4));
+            .then(bookLists => this.lists = bookLists.slice(0, 4))
+            .then(bookLists => this.getMostPopularBooks(bookLists));
     }
  }
